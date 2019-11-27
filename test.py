5e5babd9adfb4f2681fe22d199084b3281c01521
Sin <edad811@gmail.com>
2019-11-27 15:45:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,22 +7,11 @@ from torchvision.utils import save_image
 from model import MultiLevelAE
 
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-
-trans = transforms.Compose([transforms.ToTensor(),
-                            normalize])
-
-
-def denorm(tensor, device):
-    std = torch.Tensor([0.229, 0.224, 0.225]).reshape(-1, 1, 1).to(device)
-    mean = torch.Tensor([0.485, 0.456, 0.406]).reshape(-1, 1, 1).to(device)
-    res = torch.clamp(tensor * std + mean, 0, 1)
-    return res
+trans = transforms.Compose([transforms.ToTensor()])
 
 
 def main():
-    parser = argparse.ArgumentParser(description='WCT by Pytorch')
+    parser = argparse.ArgumentParser(description='WCT Style Transfer by Pytorch')
     parser.add_argument('--content', '-c', type=str, default=None,
                         help='Content image path e.g. content.jpg')
     parser.add_argument('--style', '-s', type=str, default=None,
@@ -32,7 +21,7 @@ def main():
     parser.add_argument('--alpha', '-a', type=int, default=0.6,
                         help='alpha control the fusion degree in Adain')
     parser.add_argument('--gpu', '-g', type=int, default=0,
-                        help='GPU ID(nagative value indicate CPU)')
+                        help='GPU ID(negative value indicate CPU)')
     parser.add_argument('--model_state_path', type=str, default='model_state',
                         help='save directory for result and loss')
 
@@ -55,29 +44,29 @@ def main():
     s_tensor = trans(s).unsqueeze(0).to(device)
     with torch.no_grad():
         out = model(c_tensor, s_tensor, args.alpha)
-    c_denorm = denorm(c_tensor, device)
-    out_denorm = denorm(out, device)
-    res = torch.cat([c_denorm, out_denorm], dim=0)
-    res = res.to('cpu')
 
     if args.output_name is None:
         c_name = os.path.splitext(os.path.basename(args.content))[0]
         s_name = os.path.splitext(os.path.basename(args.style))[0]
         args.output_name = f'{c_name}_{s_name}'
 
-    try:
-        save_image(out_denorm, f'{args.output_name}.jpg', nrow=1)
-        save_image(res, f'{args.output_name}_pair.jpg', nrow=2)
+    save_image(out, f'{args.output_name}.jpg', nrow=1)
+    o = Image.open(f'{args.output_name}.jpg')
 
-        o = Image.open(f'{args.output_name}_pair.jpg')
-        s = s.resize((i // 4 for i in c.size))
-        box = (o.width // 2, o.height - s.height)
-        o.paste(s, box)
-        o.save(f'{args.output_name}_style_transfer_demo.jpg', quality=95)
-        print(f'result saved into files starting with {args.output_name}')
-    except:
-        pass
+    demo = Image.new('RGB', (c.width * 2, c.height))
+    o = o.resize(c.size)
+    s = s.resize((i // 4 for i in c.size))
 
+    demo.paste(c, (0, 0))
+    demo.paste(o, (c.width, 0))
+    demo.paste(s, (c.width, c.height - s.height))
+    demo.save(f'{args.output_name}_style_transfer_demo.jpg', quality=95)
+
+    o.paste(s,  (0, o.height - s.height))
+    o.save(f'{args.output_name}_with_style_image.jpg', quality=95)
+
+    print(f'result saved into files starting with {args.output_name}')
+    
 
 if __name__ == '__main__':
     main()
